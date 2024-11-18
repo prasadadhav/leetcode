08994f30f4d9960ed3812238e71cbd34f29996ec
padhav <prasad.adhav@uni.lu>
2024-11-18 16:41:10 +0100

    daily practice 18-11-2024

---
+++ Python/leetCode_problem_11_containerWater.py
@@ -0,0 +1,63 @@
+from typing import List
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        
+        left = 0
+        right = len(height) - 1
+
+        max_water = 0
+
+        while left < right:
+
+            rect_height = min(height[left], height[right])
+            rect_width = right - left
+
+            current_water = rect_height * rect_width
+            
+            max_water = max(current_water, max_water)
+
+            if height[left] < height[right]:
+                left += 1
+                continue
+            else:
+                right -= 1
+                continue
+
+
+        return max_water
+
+height = [1,8,6,2,5,4,8,3,7]
+sol = Solution()
+
+print(sol.maxArea(height))
+
+"""
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container.
+
+ 
+
+Example 1:
+
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+Example 2:
+
+Input: height = [1,1]
+Output: 1
+ 
+
+Constraints:
+
+n == height.length
+2 <= n <= 105
+0 <= height[i] <= 104
+"""
---
+++ Python/leetCode_problem_15_3sum.py
@@ -0,0 +1,92 @@
+from typing import List
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            j = i + 1
+            k = len(nums) - 1
+
+            while j < k:
+                total = nums[i] + nums[j] + nums[k]
+
+                if total > 0:
+                    k -= 1
+                elif total < 0:
+                    j += 1
+                else:
+                    res.append([nums[i], nums[j], nums[k]])
+                    j += 1
+
+                    while nums[j] == nums[j-1] and j < k:
+                        j += 1
+        
+        return res
+
+
+"""
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        output = []
+
+        n = len(nums)
+        
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+
+                    if i != j and j != k and i != k:
+                        curr_sum = nums[i] + nums[j] + nums[k]
+
+                        if curr_sum == 0:
+                            curr_list = [nums[i], nums[j], nums[k]]
+                            curr_list.sort()
+                            if curr_list not in output:
+                                output.append(curr_list)
+
+        return output
+"""
+nums = [-1,0,1,2,-1,-4]
+sol = Solution()
+print(sol.threeSum(nums))
+
+
+"""
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+ 
+
+Constraints:
+
+3 <= nums.length <= 3000
+-105 <= nums[i] <= 105
+"""
